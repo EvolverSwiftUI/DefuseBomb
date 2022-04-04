17869e07a5fdeafe9c59579ccbdcd163f32b81ba
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-05 00:45:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let timerEl = document.getElementById("timer");
+let defuserEl = document.getElementById("defuser");
+
+let countDown = 10;
+let intervalId = setInterval(function() {
+    countDown = countDown - 1;
+    timerEl.textContent = countDown;
+    if (countDown === 0) {
+        timerEl.textContent = "BOOM!!!";
+        clearInterval(intervalId);
+    }
+}, 1000);
+
+defuserEl.addEventListener("keydown", function(event){
+    let bombDefuserText = defuserEl.value;
+    if (event.key === "Enter" && bombDefuserText === "defuse" && countDown !== 0)  {
+        timerEl.textContent = "You did it!!!"
+        clearInterval(intervalId);
+    }
+});
+
